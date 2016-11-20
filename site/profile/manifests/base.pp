b0e48aead52f9cd7a3268778a42ba1959bea6425
alemseged adila <alemseged.adila@ju.edu.et>
2016-11-21 00:52:50 +0100

    puppetizing /etc/hosts file

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -7,6 +7,16 @@ class profile::base {
     servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
   }
 
+  class { '::hosts':
+    one_primary_ipv4 => false,
+    one_primary_ipv6 => false,
+    entries => {
+      '127.0.0.1'     => ['localhost'],
+      '192.168.33.20' => ['pmaster.home', 'pmaster.guest.ugent.be', 'pmaster', 'puppet'],
+      '192.168.33.30' => ['pclient.home', 'pclient.guest.ugent.be', 'pclient'],
+      '192.168.33.40' => ['pclient2.home', 'pclient2.guest.ugent.be', 'pclient2']
+    }
+
   include '::vim'
 
   # apt repositories 
