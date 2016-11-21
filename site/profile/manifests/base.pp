4b21c87e768675dfa87849113f87504df7e6431d
alemseged adila <alemseged.adila@ju.edu.et>
2016-11-21 01:03:18 +0100

    parameter changed for hosts module

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -8,15 +8,8 @@ class profile::base {
   }
 
   class { '::hosts':
-    one_primary_ipv4 => false,
-    one_primary_ipv6 => false,
-    entries => {
-      '127.0.0.1'     => ['localhost'],
-      '192.168.33.20' => ['pmaster.home', 'pmaster.guest.ugent.be', 'pmaster', 'puppet'],
-      '192.168.33.30' => ['pclient.home', 'pclient.guest.ugent.be', 'pclient'],
-      '192.168.33.40' => ['pclient2.home', 'pclient2.guest.ugent.be', 'pclient2']
-    }
-}
+    dynamic_mode => true,   
+  }
 
   include '::vim'
 
