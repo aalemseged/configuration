55fe0ea6996d8d2db1bcde11b8def60da44a38c5
alemseged adila <alemseged.adila@ju.edu.et>
2016-11-22 16:33:39 +0100

    two users added on the manageuser profile

--- site/profile/manifests/manageusers.pp
+++ site/profile/manifests/manageusers.pp
@@ -1,5 +1,21 @@
 class profile::manageusers {
 
+  accounts::user { 'ebsuser1':
+    uid      => 4001,
+    gid      => 4001,
+    shell    => '/bin/bash',
+    password => '!!',
+    sshkeys  => "ssh-rsa AAAA...",
+    locked   => false,
+  }
 
+  accounts::user { 'ebsuser2':
+    uid      => 4002,
+    gid      => 4002,
+    shell    => '/bin/bash',
+    password => '!!',
+    sshkeys  => "ssh-rsa AAAA...",
+    locked   => false,
+  }
 
 }
