1cb2a343a04bacd6059a3d932670983c6e21a952
alemseged adila <alemseged.adila@ju.edu.et>
2016-11-22 16:40:11 +0100

    parameter values stringed

--- site/profile/manifests/manageusers.pp
+++ site/profile/manifests/manageusers.pp
@@ -1,21 +1,21 @@
 class profile::manageusers {
 
   accounts::user { 'ebsuser1':
-    uid      => 4001,
-    gid      => 4001,
+    uid      => '4001',
+    gid      => '4001',
     shell    => '/bin/bash',
     password => '!!',
     #sshkeys  => "ssh-rsa AAAA...",
-    locked   => false,
+    locked   => 'false',
   }
 
   accounts::user { 'ebsuser2':
-    uid      => 4002,
-    gid      => 4002,
+    uid      => '4002',
+    gid      => '4002',
     shell    => '/bin/bash',
     password => '!!',
     #sshkeys  => "ssh-rsa AAAA...",
-    locked   => false,
+    locked   => 'false',
   }
 
 }
