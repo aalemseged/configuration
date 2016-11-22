ae310e1b8bc2b44cbe886be1462f3d6112bcf240
alemseged adila <alemseged.adila@ju.edu.et>
2016-11-22 10:26:28 +0100

    nfs module removed

--- site/profile/manifests/easybuildnfs.pp
+++ site/profile/manifests/easybuildnfs.pp
@@ -1,11 +1,11 @@
 class profile::easybuildnfs {
 
-  class { '::nfs':
-    server_enabled => false,
-    client_enabled => true,
-    nfs_v4_client  => false,
-  }
+  #class { '::nfs':
+  #  server_enabled => false,
+  #  client_enabled => true,
+  #  nfs_v4_client  => false,
+  #}
   
-  Nfs::Client::Mount <<| |>>
+  #Nfs::Client::Mount <<| |>>
 
 }
--- site/profile/manifests/hpcnfserver.pp
+++ site/profile/manifests/hpcnfserver.pp
@@ -6,14 +6,14 @@ class profile::hpcnfserver {
     shell  => '/bin/bash',
   }
 
-  class { '::nfs':
-    server_enabled => true,
-    nfs_v4	   => false,  
-    exports_file   => '/etc/exports'
-  }
+  #class { '::nfs':
+  #  server_enabled => true,
+  #  nfs_v4	   => false,  
+  #  exports_file   => '/etc/exports'
+  #}
 
-  nfs::server::export { '/home/vagrant/software/':
-    ensure  => 'mounted',
-    clients => '*(rw,insecure,async,no_root_squash) localhost(rw)'
-  }
+  #nfs::server::export { '/home/vagrant/software/':
+  #  ensure  => 'mounted',
+  #  clients => '*(rw,insecure,async,no_root_squash) localhost(rw)'
+  #}
 }
