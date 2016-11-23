9dc12298499506527b5e73b03d44af25bc4bdbd6
alemseged adila <alemseged.adila@ju.edu.et>
2016-11-23 16:08:03 +0100

    users created with default params and values

--- site/profile/manifests/manageusers.pp
+++ site/profile/manifests/manageusers.pp
@@ -1,22 +1,8 @@
 class profile::manageusers {
 
-  accounts::user { 'ebsuser1':
-    uid      => '4001',
-    gid      => '4001',
-    shell    => '/bin/bash',
-    password => '$1$WAWf/Dao$9r6e/LpCPI4yZ8aAQ2vSe',
-    #sshkeys  => "ssh-rsa AAAA...",
-    locked   => false,
-  }
+  accounts::user { 'ebsuser1': }
 
-  accounts::user { 'ebsuser2':
-    uid      => '4002',
-    gid      => '4002',
-    shell    => '/bin/bash',
-    password => '$1$w35mXEdD$D2bQ0nzTk7gdRW2gwJLQ7',
-    #sshkeys  => "ssh-rsa AAAA...",
-    locked   => false,
-  }
+  accounts::user { 'ebsuser2': }
 
   accounts::user { 'hpcadmin':
     ensure   => present,
